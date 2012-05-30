2e9eaed19a977bf44c9b6bbc21b7db6ffad956bd
Ben Osheroff <ben@zendesk.com>
2012-05-30 10:34:55 -0700

    patch into bundler around specs instead of add_remote

--- lib/rubygems_over_ssh.rb
+++ lib/rubygems_over_ssh.rb
@@ -2,11 +2,12 @@ require 'net/ssh/gateway'
 
 module RubyGemsOverSSH
   class << self
-    def init_tunnel(host)
+    def init_tunnel(host, port)
       @tunnels ||= {}
-      @tunnels[host] ||= begin
+      @tunnels["#{host}:#{port}"] ||= begin
+        puts "rubygemsoverssh: opening tunnel to #{host}:#{port}"
         gateway = Net::SSH::Gateway.new(host, `whoami`.chomp)
-        gateway.open("localhost", 80, 9080)
+        gateway.open("localhost", port, 9080)
       end
       9080
     end
@@ -17,15 +18,21 @@ if Object.const_defined?(:Bundler)
   module Bundler
     module Source
       class Rubygems
-        def add_remote(source)
-          if source =~ %r{^gem\+ssh://(.*)}
-            puts "opening tunnel to #{$1}"
-            port = RubyGemsOverSSH.init_tunnel($1)
-            source = "http://localhost:#{port}"
-          end
+        alias :specs_without_ssh :specs
 
-          puts source
-          @remotes << normalize_uri(source)
+        def specs(*args, &block)
+          preserve_remotes = @remotes
+          @remotes = @remotes.map { |r|
+            if r.scheme == "ssh+http"
+              local_port = RubyGemsOverSSH.init_tunnel(r.host, r.port)
+              URI.parse("http://localhost:#{local_port}/")
+            else
+              r
+            end
+          }
+          specs_without_ssh(*args, &block)
+        ensure
+          @remotes = preserve_remotes
         end
       end
     end
