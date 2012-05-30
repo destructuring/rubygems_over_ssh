2996c5621c6ddf28d527412bb1856b2284a1cd52
osheroff <ben@zendesk.com>
2012-05-30 11:36:38 -0700

    randomize local port, support Geminabox

--- lib/rubygems_over_ssh.rb
+++ lib/rubygems_over_ssh.rb
@@ -1,15 +1,30 @@
 require 'net/ssh/gateway'
+require 'uri'
 
 module RubyGemsOverSSH
   class << self
-    def init_tunnel(host, port)
-      @tunnels ||= {}
-      @tunnels["#{host}:#{port}"] ||= begin
-        puts "rubygemsoverssh: opening tunnel to #{host}:#{port}"
-        gateway = Net::SSH::Gateway.new(host, `whoami`.chomp)
-        gateway.open("localhost", port, 9080)
+    def open_tunnel(gateway, remote_port)
+      begin
+        try_port = 1025 + rand(60_000)
+        gateway.open("localhost", remote_port, try_port)
+        return try_port
+      rescue Errno::EADDRINUSE
+        retry
       end
-      9080
+    end
+
+    def init_tunnel(host, port)
+      local_port = nil
+      port ||= 80
+      key = host + ":" + port.to_s
+      @tunnels ||= {}
+      @tunnels[key] ||= begin
+        gateway = Net::SSH::Gateway.new(host, `whoami`.chomp)
+        local_port = open_tunnel(gateway, port)
+        puts "rubygemsoverssh: opened tunnel to #{key} (listening locally on #{local_port})"
+        {:gateway => gateway, :local_port => local_port}
+      end
+      @tunnels[key][:local_port]
     end
   end
 end
@@ -35,6 +50,27 @@ if Object.const_defined?(:Bundler)
           @remotes = preserve_remotes
         end
       end
+    end
+  end
+end
+
+begin
+  require 'rubygems/commands/inabox_command'
+rescue
+end
+
+if Gem::Commands.const_defined?(:InaboxCommand)
+  class Gem::Commands::InaboxCommand < Gem::Command
+    alias_method :geminabox_host_without_tunnel, :geminabox_host
+
+    def geminabox_host
+      host = geminabox_host_without_tunnel
+      uri = URI.parse(host)
+      if uri.scheme == "ssh+http"
+        local_port = RubyGemsOverSSH.init_tunnel(uri.host, uri.port)
+        host = "http://localhost:#{local_port}/"
+      end
+      host
     end
   end
 end
